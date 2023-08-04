185f13f36dbbba16edead40c7f9c01db899ed30a
Andres Duque <andres.duquec@pragma.com.co>
2023-08-04 09:32:44 -0500

    Initial commit

---
+++ StarWarsWiki/StarWarsWiki/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StarWarsWiki
+//
+//  Created by Andres Duque on 4/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsWiki/StarWarsWiki/StarWarsWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsWikiApp.swift
+//  StarWarsWiki
+//
+//  Created by Andres Duque on 4/08/23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
